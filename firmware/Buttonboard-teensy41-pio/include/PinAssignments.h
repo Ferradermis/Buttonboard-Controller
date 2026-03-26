3481c24204f22799c98accefcbd9787fd4a497f5
Andy McKenzie <ammckenzie@gmail.com>
2026-03-26 15:49:06 -0500

    refactored pin assignments

---
+++ firmware/Buttonboard-teensy41-pio/include/PinAssignments.h
@@ -0,0 +1,45 @@
+#ifndef PIN_ASSIGNMENTS_H
+#define PIN_ASSIGNMENTS_H
+
+// Button Pins
+#define PIN_BTN_01 0
+#define PIN_BTN_02 1
+#define PIN_BTN_03 2
+#define PIN_BTN_04 3
+#define PIN_BTN_05 4
+#define PIN_BTN_06 5
+#define PIN_BTN_07 6
+#define PIN_BTN_08 7
+#define PIN_BTN_09 8
+#define PIN_BTN_10 9
+#define PIN_BTN_11 10
+#define PIN_BTN_12 11
+#define PIN_BTN_13 12
+#define PIN_BTN_14 26
+#define PIN_BTN_15 27
+#define PIN_BTN_16 28
+#define PIN_BTN_17 29
+#define PIN_BTN_18 30
+#define PIN_BTN_19 31
+#define PIN_BTN_20 32
+#define PIN_BTN_21 33
+#define PIN_BTN_22 34
+#define PIN_BTN_23 35
+#define PIN_BTN_24 36
+#define PIN_BTN_25 38
+#define PIN_BTN_26 39
+#define PIN_BTN_27 40
+#define PIN_BTN_28 41
+
+// NeoPixel Data Pin
+#define DATA_PIN 37
+
+// Joystick Axis Pins
+#define PIN_JOY_X 14
+#define PIN_JOY_Y 15
+#define PIN_JOY_Z 20
+#define PIN_JOY_ZR 21
+#define PIN_JOY_S1 22
+#define PIN_JOY_S2 23
+
+#endif // PIN_ASSIGNMENTS_H
--- firmware/Buttonboard-teensy41-pio/src/main.cpp
+++ firmware/Buttonboard-teensy41-pio/src/main.cpp
@@ -1,67 +1,20 @@
 // Add this to the top of your main.cpp (after other includes)
-#include "TeensyUDPClient.h"
-#include "NativeEthernet.h"
 #include <Bounce2.h>
 #include <FastLED.h>
 #include <IntervalTimer.h>
 #include <LiquidCrystal_I2C.h>
+#include "PinAssignments.h"
 
-
-#pragma region Button Pins
-#define PIN_BTN_01 0
-#define PIN_BTN_02 1
-#define PIN_BTN_03 2
-#define PIN_BTN_04 3
-#define PIN_BTN_05 4
-#define PIN_BTN_06 5
-#define PIN_BTN_07 6
-#define PIN_BTN_08 7
-#define PIN_BTN_09 8
-#define PIN_BTN_10 9
-#define PIN_BTN_11 10
-#define PIN_BTN_12 11
-#define PIN_BTN_13 12
-#define PIN_BTN_14 26
-#define PIN_BTN_15 27
-#define PIN_BTN_16 28
-#define PIN_BTN_17 29
-#define PIN_BTN_18 30
-#define PIN_BTN_19 31
-#define PIN_BTN_20 32
-#define PIN_BTN_21 33
-#define PIN_BTN_22 34
-#define PIN_BTN_23 35
-#define PIN_BTN_24 36
-#define PIN_BTN_25 38
-#define PIN_BTN_26 39
-#define PIN_BTN_27 40
-#define PIN_BTN_28 41
-#pragma endregion
-
-#pragma region Joystick Axis Pins
-#define PIN_JOY_X 14
-#define PIN_JOY_Y 15
-#define PIN_JOY_Z 20
-#define PIN_JOY_ZR 21
-#define PIN_JOY_S1 22
-#define PIN_JOY_S2 23
-#pragma endregion
 
 
 #define PIN_LED 13
 #define NUM_LEDS 6
-#define DATA_PIN 37
+
 #define STATES_PER_LED 4 // Each LED can have 8 animation states (on/off, or different colors)
 
 
 const uint8_t _buttonPins[] = {
   PIN_BTN_01,PIN_BTN_02,PIN_BTN_03,PIN_BTN_04,PIN_BTN_05,PIN_BTN_06
-  /*,
-  PIN_BTN_07,PIN_BTN_08,PIN_BTN_09,PIN_BTN_10,PIN_BTN_11,PIN_BTN_12,
-  PIN_BTN_13,PIN_BTN_14,PIN_BTN_15,PIN_BTN_16,PIN_BTN_17,PIN_BTN_18,
-  PIN_BTN_19,PIN_BTN_20,PIN_BTN_21,PIN_BTN_22,PIN_BTN_23,PIN_BTN_24,
-  PIN_BTN_25,PIN_BTN_26,PIN_BTN_27,PIN_BTN_28
-  */
 };
 
 const uint8_t numButtons = 6; //sizeof(_buttonPins) / sizeof(_buttonPins[0]);
@@ -79,15 +32,11 @@ CRGB algaeColor=CRGB::SeaGreen;
 
 
 
-void SetupEthernet();
 
 void SetLEDColors(int ledIndex, std::initializer_list<CRGB> colors);
 void SetLEDColor(int ledIndex, CRGB color);
 void CheckButtonStates();
 void transferColors();
-void SetLEDColorFromTagId(int tagId, CRGB color);
-void SetLEDColorFromTagIdLeft(int tagId, CRGB color);
-void SetLEDColorFromTagIdRight(int tagId, CRGB color);
 
 // LED test helper (declared here for PlatformIO builds)
 void RunLEDTestAnimation();
@@ -322,26 +271,6 @@ void SetLEDColor(int ledIndex, CRGB color) {
     
 }
 
-
-void SetupEthernet(){
-    // Simple ethernet setup
-    byte mac[6] = {0x02, 0xFE, 0xED, 0x65, 0x74, 0x64};
-    Serial.println("Starting Ethernet...");
-    
-    if (Ethernet.begin(mac,15000,5000)) {
-        Serial.println("✅ Ethernet configured via DHCP");
-    } else {
-        // Try static IP
-        IPAddress ip(10, 65, 74, 100);  // Team 6574 static IP
-        IPAddress gateway(10, 65, 74, 1);
-        IPAddress subnet(255, 255, 255, 0);
-        Ethernet.begin(mac, ip, gateway, gateway, subnet);
-        Serial.println("✅ Ethernet configured with static IP");
-    }
-    
-    Serial.print("Local IP: ");
-    Serial.println(Ethernet.localIP());
-}
 
 void isr_animationTimer(){
     animationFrame = ++animationFrame % STATES_PER_LED; // Toggle frame 0 to STATES_PER_LED-1
